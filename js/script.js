c232ec42b00294bc579ce367678dc02b0e967a22
Mosab Ahmed Irwished <42383497+Mosab97@users.noreply.github.com>
2024-05-19 02:43:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,388 @@
+(function($) {
+	
+	"use strict";
+	$(window).on('load',function(){
+		$("#preloader").fadeOut();
+		wow = new WOW(
+			{
+				animateClass: 'animated',
+				offset:       50
+			}
+		);
+		wow.init();
+	});
+
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.preloader').length){
+			$('.preloader').delay(200).fadeOut(500);
+		}
+	}
+	
+	//Update Header Style and Scroll to Top
+	function headerStyle() {
+		if($('.main-header').length){
+			var windowpos = $(window).scrollTop();
+			var siteHeader = $('.main-header');
+			var scrollLink = $('.scroll-top');
+			if (windowpos >= 110) {
+				siteHeader.addClass('fixed-header');
+				scrollLink.addClass('open');
+			} else {
+				siteHeader.removeClass('fixed-header');
+				scrollLink.removeClass('open');
+			}
+		}
+	}
+	
+	headerStyle();
+
+
+	//Submenu Dropdown Toggle
+	if($('.main-header li.dropdown ul').length){
+		$('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><span class="fa fa-angle-right"></span></div>');
+		
+	}
+
+	//Mobile Nav Hide Show
+	if($('.mobile-menu').length){
+		
+		$('.mobile-menu .menu-box').mCustomScrollbar();
+		
+		var mobileMenuContent = $('.main-header .menu-area .main-menu').html();
+		$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+		$('.sticky-header .main-menu').append(mobileMenuContent);
+		
+		//Dropdown Button
+		$('.mobile-menu li.dropdown .dropdown-btn').on('click', function() {
+			$(this).toggleClass('open');
+			$(this).prev('ul').slideToggle(500);
+		});
+		//Menu Toggle Btn
+		$('.mobile-nav-toggler').on('click', function() {
+			$('body').addClass('mobile-menu-visible');
+		});
+
+		//Menu Toggle Btn
+		$('.mobile-menu .menu-backdrop,.mobile-menu .close-btn').on('click', function() {
+			$('body').removeClass('mobile-menu-visible');
+		});
+	}
+
+
+	// Scroll to a Specific Div
+	if($('.scroll-to-target').length){
+		$(".scroll-to-target").on('click', function() {
+			var target = $(this).attr('data-target');
+		   // animate
+		   $('html, body').animate({
+			   scrollTop: $(target).offset().top
+			 }, 1000);
+	
+		});
+	}
+
+	// Elements Animation
+	if($('.wow').length){
+		var wow = new WOW({
+		mobile:       false
+		});
+		wow.init();
+	}
+
+
+
+    //11.progressBarConfig
+	function progressBarConfig () {
+	  	var progressBar = $('.progress');
+	  		if(progressBar.length) {
+	    		progressBar.each(function () {
+	      		var Self = $(this);
+	      		Self.appear(function () {
+	        	var progressValue = Self.data('value');
+
+	        	Self.find('.progress-bar').animate({
+	          	width:progressValue+'%'           
+	        	}, 100);
+
+	        	Self.find('span.value').countTo({
+	          		from: 0,
+	            	to: progressValue,
+	            	speed: 100
+	        		});
+	     	 	});
+	   		})
+	  	}
+	}
+
+
+	//Sortable Masonary with Filters
+	function enableMasonry() {
+		if($('.sortable-masonry').length){
+	
+			var winDow = $(window);
+			// Needed variables
+			var $container=$('.sortable-masonry .items-container');
+			var $filter=$('.filter-btns');
+	
+			$container.isotope({
+				filter:'*',
+				 masonry: {
+					columnWidth : '.masonry-item.small-column'
+				 },
+				animationOptions:{
+					duration:500,
+					easing:'linear'
+				}
+			});
+			
+	
+			// Isotope Filter 
+			$filter.find('li').on('click', function(){
+				var selector = $(this).attr('data-filter');
+	
+				try {
+					$container.isotope({ 
+						filter	: selector,
+						animationOptions: {
+							duration: 500,
+							easing	: 'linear',
+							queue	: false
+						}
+					});
+				} catch(err) {
+	
+				}
+				return false;
+			});
+	
+	
+			winDow.on('resize', function(){
+				var selector = $filter.find('li.active').attr('data-filter');
+
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 500,
+						easing	: 'linear',
+						queue	: false
+					}
+				});
+			});
+	
+	
+			var filterItemA	= $('.filter-btns li');
+	
+			filterItemA.on('click', function(){
+				var $this = $(this);
+				if ( !$this.hasClass('active')) {
+					filterItemA.removeClass('active');
+					$this.addClass('active');
+				}
+			});
+		}
+	}
+	
+	enableMasonry();
+
+
+	// Select menu 
+	function selectDropdown() {
+	    if ($(".selectmenu").length) {
+	        $(".selectmenu").selectmenu();
+
+	        var changeSelectMenu = function(event, item) {
+	            $(this).trigger('change', item);
+	        };
+	        $(".selectmenu").selectmenu({ change: changeSelectMenu });
+	    };
+	}
+// testimonial-carousel
+	if ($('.testimonial-carousel-2').length) {
+		$('.testimonial-carousel-2').owlCarousel({
+			loop:true,
+			margin:30,
+			nav:false,
+			smartSpeed: 3000,
+			autoplay: true,
+			navText: [ '<span class="flaticon-left"></span>', '<span class="flaticon-right"></span>' ],
+			responsive:{
+				0:{
+					items:1
+				},
+				480:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				800:{
+					items:1
+				},
+				1200:{
+					items:1
+				}
+
+			}
+		});
+	}
+
+
+	$(document).ready(function(){
+		// Add smooth scrolling to all links
+		$("a").on('click', function(event) {
+
+			// Make sure this.hash has a value before overriding default behavior
+			if (this.hash !== "") {
+				// Prevent default anchor click behavior
+				event.preventDefault();
+
+				// Store hash
+				var hash = this.hash;
+
+				// Using jQuery's animate() method to add smooth page scroll
+				// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				}, 800, function(){
+
+					// Add hash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;
+				});
+			} // End if
+		});
+	});
+
+
+	//single-item-carousel
+	if ($('.single-item-carousel').length) {
+		$('.single-item-carousel').owlCarousel({
+			loop:true,
+			margin:0,
+			nav:true,
+			animateOut: 'fadeOut',
+    		animateIn: 'fadeIn',
+    		active: true,
+			smartSpeed: 1000,
+			autoplay: 6000,
+			navText: [ '<span class="flaticon-left-arrow"></span>', '<span class="flaticon-right-arrow"></span>' ],
+			dots: true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1024:{
+					items:1
+				}
+			}
+		});     		
+	}
+
+
+
+
+	if($('.paroller').length){
+		$('.paroller').paroller({
+			  factor: 0.05,            // multiplier for scrolling speed and offset, +- values for direction control  
+			  factorLg: 0.05,          // multiplier for scrolling speed and offset if window width is less than 1200px, +- values for direction control  
+			  type: 'foreground',     // background, foreground  
+			  direction: 'horizontal' // vertical, horizontal  
+		});
+	}
+
+
+	// 6 pieChart RoundCircle
+	function expertizeRoundCircle () {
+		var rounderContainer = $('.piechart');
+		if (rounderContainer.length) {
+			rounderContainer.each(function () {
+				var Self = $(this);
+				var value = Self.data('value');
+				var size = Self.parent().width();
+				var color = Self.data('fg-color');
+
+				Self.find('span').each(function () {
+					var expertCount = $(this);
+					expertCount.appear(function () {
+						expertCount.countTo({
+							from: 1,
+							to: value*100,
+							speed: 3000
+						});
+					});
+
+				});
+				Self.appear(function () {					
+					Self.circleProgress({
+						value: value,
+						size: 270,
+						thickness: 30,
+						emptyFill: '#e1e1e1',
+						animation: {
+							duration: 3000
+						},
+						fill: {
+							color: color
+						}
+					});
+				});
+			});
+		};
+	}
+
+
+
+	function onHoverthreeDmovement() {
+	    var tiltBlock = $('.js-tilt');
+	    if(tiltBlock.length) {
+	        $('.js-tilt').tilt({
+	            maxTilt: 20,
+	            perspective:700, 
+	            glare: true,
+	            maxGlare: 0
+	        })
+	    }
+	}
+
+
+	/*	=========================================================================
+	When document is Scrollig, do
+	========================================================================== */
+
+	jQuery(document).on('ready', function () {
+		(function ($) {
+			// add your functions
+			progressBarConfig ();
+			selectDropdown();
+			onHoverthreeDmovement();
+		})(jQuery);
+	});
+
+
+
+	/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+	
+	$(window).on('scroll', function() {
+		headerStyle();
+	});
+
+	
+	
+	/* ==========================================================================
+   When document is loaded, do
+   ========================================================================== */
+	
+	$(window).on('load', function() {
+		handlePreloader();
+		enableMasonry();
+		expertizeRoundCircle ();
+	});
+
+	
+
+})(window.jQuery);
